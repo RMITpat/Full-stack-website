ca44c156d040c74d82d77d05e1377b6ae40ca20c
Lach <s3976017@student.rmit.edu.au>
2025-05-09 19:08:13 +1000

    updated user entity and added App_Credential entity is now runs in dev

--- backend/src/entity/User.ts
+++ backend/src/entity/User.ts
@@ -2,9 +2,23 @@ import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
+  OneToOne,
+  JoinColumn,
   CreateDateColumn,
   UpdateDateColumn,
 } from "typeorm";
+import { App_Credential } from "./App_Credential";
+
+export enum user_type {
+   LOGGEDOUT        = "default", 
+   LOGGEDIN         = "logged_in", 
+   LOGGEDINLECTURER = "logged_in_lecturer", 
+   ADMINDEFAULT     = "admin_default", 
+   ADMINLOGGEDIN    = "admin_logged_in", 
+   ADMINLECTURER    = "admin_lecturer"
+
+}
+
 
 @Entity()
 export class User {
@@ -14,18 +28,23 @@ export class User {
   @Column()
   firstName: string;
 
-  @Column()
-  lastName: string;
-
   @Column({ unique: true })
   email: string;
 
-  @Column()
-  age: number;
+  @Column({
+    type: "enum",
+    enum: user_type,
+    default: user_type.LOGGEDOUT,
+  })
+  type: user_type;
 
   @CreateDateColumn()
   createdAt: Date;
 
   @UpdateDateColumn()
-  updatedAt: Date;
+  updatedAt: Date
+
+  @OneToOne(() => App_Credential)
+  @JoinColumn()
+  cred: App_Credential;
 }
